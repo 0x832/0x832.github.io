d9f1d15308a5fc505b3ee57a5d47ca915d3390ed
0X832 <143940631+0x832@users.noreply.github.com>
2024-08-01 22:59:13 +0200

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "SnapBot - Python scripting",
+        "excerpt":"Hola a todos, hoy les presentaré un pequeño script en Python que desarrollé hace unos días, llamado SnapBot . Este programa está diseñado para capturar imágenes utilizando la cámara web de tu ordenador y enviarlas directamente a un chat de Telegram que hayas configurado previamente. Requisitos Primero, necesitaremos crear tres...","categories": ["Python","Programación"],
+        "tags": ["Python","Programación","Srcripting"],
+        "url": "http://localhost:4000/snapbot-python-scripting/",
+        "teaser":"http://localhost:4000/assets/images/script-python-SnapBot/SnapBot1.jpg"}]
