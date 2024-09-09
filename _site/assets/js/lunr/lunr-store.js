0e5953397ecdee2633dea72ab363618a5c5907c9
0X832 <143940631+0x832@users.noreply.github.com>
2024-09-09 14:34:21 +0200

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "SnapBot - Python scripting",
+        "excerpt":"Hola a todos, hoy enseñaré un pequeño script en Python que desarrollé hace unos días, llamado SnapBot . Este programa está echo para capturar una imágen utilizando la cámara web de tu ordenador y enviarlas a un chat de Telegram que hayas configurado anteriormente. Requisitos Primero, necesitaremos crear tres archivos:...","categories": ["Programación"],
+        "tags": ["Python","Programación"],
+        "url": "http://localhost:4000/snapbot-python-scripting/",
+        "teaser":"http://localhost:4000/assets/images/script-python-SnapBot/SnapBot1.jpg"},{
+        "title": "Runner - Medium",
+        "excerpt":"Hola a todos, hoy les presentaré la resolución de una máquina Hack the box de dificultad Media. En esta máquina aprenderemos a explotar una vulnerabilidad en TeamCity 2023.05.3 para posteriormente conectarnos por SSH y llegar a ser root aprovechando una versión obsoleta de runC (versión 1.1.7). Reconocimiento Enumeración con nmap...","categories": ["Hack the box","Pentesting","CTF"],
+        "tags": ["TeamCity 2023.05.3","SSH","runC (versión 1.1.7)"],
+        "url": "http://localhost:4000/runner-medium/",
+        "teaser":"http://localhost:4000/assets/images/2024-08-07-runner-medium/Runner.png"}]
