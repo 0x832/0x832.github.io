55401f7b0f9a7ca195142fc931ac69abe59388e8
0X832 <143940631+0x832@users.noreply.github.com>
2026-07-13 15:32:09 +0200

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,41 @@
+var store = [{
+        "title": "SnapBot - Python scripting",
+        "excerpt":"Hola a todos, hoy enseñaré un pequeño script en Python que desarrollé hace unos días, llamado SnapBot . Este programa está echo para capturar una imágen utilizando la cámara web de tu ordenador y enviarlas a un chat de Telegram que hayas configurado anteriormente. Requisitos Primero, necesitaremos crear tres archivos:...","categories": ["Herramientas","Programación"],
+        "tags": ["Python","Telegram Bot","Webcam","Automatización"],
+        "url": "http://0.0.0.0:4000/snapbot-python-scripting/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2024-07-20-script-python-SnapBot/SnapBot1.jpg"},{
+        "title": "Editorial - Easy",
+        "excerpt":"Hola a todos, hoy les presentaré la resolución de una máquina Hack the box de dificultad Easy. En esta máquina aprenderemos a detectar un SSRF para luego conectarnos por SSH y llegar a ser root abusando de privilegios a nivel de Sudoers” Reconocimiento Enumeración con nmap Hacemos un nmap simple...","categories": ["Writeups","Hack The Box"],
+        "tags": ["SSRF","SSH","Sudoers","Linux"],
+        "url": "http://0.0.0.0:4000/editorial-easy/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2024-08-07-editorial-easy/Editorial.png"},{
+        "title": "Boardlight - Easy",
+        "excerpt":"Hola a todos, hoy les presentaré la resolución de una máquina Hack the box de dificultad Easy. En esta máquina aprenderemos a explotar Dolibar v17.0.0 con un exploit para luego conectarnos por SSH y llegar a ser root abusando de privilegios SUID con otro exploit Reconocimiento Enumeración con nmap Hacemos...","categories": ["Writeups","Hack The Box"],
+        "tags": ["Dolibarr","Exploit","SUID","Linux"],
+        "url": "http://0.0.0.0:4000/boardlight-easy/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2024-08-09-boardlight-easy/BoardLight.png"},{
+        "title": "Runner - Medium",
+        "excerpt":"Hola a todos, hoy les presentaré la resolución de una máquina Hack the box de dificultad Media. En esta máquina aprenderemos a explotar una vulnerabilidad en TeamCity 2023.05.3 para posteriormente conectarnos por SSH y llegar a ser root aprovechando una versión obsoleta de runC (versión 1.1.7). Reconocimiento Enumeración con nmap...","categories": ["Writeups","Hack The Box"],
+        "tags": ["TeamCity","CVE-2024-21626","SSH","Docker","runC","Linux"],
+        "url": "http://0.0.0.0:4000/runner-medium/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2024-08-07-runner-medium/Runner.png"},{
+        "title": "Command & Control - Python scripting",
+        "excerpt":"Hola a todos, hoy les presentaré un pequeño script en Python que desarrollé hace unos días, llamado Command and Control. Su función principal es permitir la ejecución remota de diferentes funciones del sistema Windows a través de un bot de Telegram Requisitos Necesitaremos tener BotFeather, Get My ID BotFeather: Utilizaremos...","categories": ["Herramientas","Programación"],
+        "tags": ["Python","Telegram Bot","C2","Capturas","Automatización"],
+        "url": "http://0.0.0.0:4000/command-control/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2024-10-29-command-control/telegram.jpg"},{
+        "title": "Chemistry - Easy",
+        "excerpt":"Hola a todos, hoy les presentaré la resolución de una máquina de Hack The Box de dificultad fácil. En esta máquina aprenderemos a subir un archivo CIF malicioso para luego acceder al sistema y, a continuación, acceder por SSH con un usuario y llegar a ser root abusando de LFI....","categories": ["Writeups","Hack The Box"],
+        "tags": ["CIF","LFI","Python","Linux"],
+        "url": "http://0.0.0.0:4000/Chemistry-easy/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2024-11-24-chemistry-easy/Chemistry.png"},{
+        "title": "Liar (Windows) Writeup",
+        "excerpt":"Hola a todos, hoy les presentaré la resolución de una máquina HackMyVM de dificultad Easy. En esta máquina aprenderemos a enumerar y explotar el servicio SMB, y también a utilizar WinRM para obtener acceso remoto en el sistema Windows, una vez que tengamos las credenciales correctas. Reconocimiento de la Red...","categories": ["Writeups","HackMyVM"],
+        "tags": ["Windows","SMB","WinRM","Privilege Escalation"],
+        "url": "http://0.0.0.0:4000/HakckMyVM-Lia-easy/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2025-04-13-HackMyVM-Lia-easy/2.png"},{
+        "title": "Evil IntrudeR - Bash Script para Auditoría Wi-Fi",
+        "excerpt":"Introducción Hoy presentaré el análisis de una herramienta que desarrollé en Bash para automatizar tareas de auditoría Wi-Fi con airmon-ng, macchanger, john y cowpatty. Explicaré de forma breve cómo funciona esta herramienta y sus funciones Este script reúne y automatiza herramientas como: airmon-ng → Activar modo monitor. airodump-ng → Captura...","categories": ["Herramientas","Wi-Fi"],
+        "tags": ["Bash","Auditoría Wi-Fi","Aircrack-ng","Automatización"],
+        "url": "http://0.0.0.0:4000/Evil-Intruder-bash-scripting/",
+        "teaser":"http://0.0.0.0:4000/assets/images/2025-11-11-Evil-Intruder-python-scripting/evil-intruder.png"}]
